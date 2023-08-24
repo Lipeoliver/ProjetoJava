129b5c9a3845846a2c6a020f0cfaaaeb1abd98fa
Felipe Oliveira <andre_felipe200915@hotmail.com>
2023-08-24 15:27:45 -0300

    calculadoras

---
+++ SomaSwing/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"lipe.oliveira","c":"TelaSoma","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"lipe.oliveira","c":"TelaSoma","l":"TelaSoma()","u":"%3Cinit%3E()"}];updateSearchResults();
